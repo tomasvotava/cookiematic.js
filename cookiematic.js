dc98c9dadce153bae79e333c1ed7af872f66fe08
Tomas Votava <tomas.votava@bizztreat.com>
2022-02-10 16:21:17 +0100

    policy text

--- cookiematic.js
+++ cookiematic.js
@@ -21,6 +21,7 @@ function getCookie(cookiename) {
  * @param {String} name Cookie name to set value of
  * @param {String} value value for a new cookie
  * @param {Number} expDays number of days for expiration date (default: 30)
+ * @param {String} site site url to be displayed in privacy policy text
  */
 function setCookie(name, value, expDays) {
   const exp = new Date();
@@ -37,6 +38,8 @@ function setCookie(name, value, expDays) {
 /**
  * @typedef {Object} CookieMaticOptions
  * @property {String} accentColor
+ * @property {String} accentText
+ * @property {Boolean} reload
  */
 
 class CookieMatic {
@@ -46,6 +49,7 @@ class CookieMatic {
    */
   constructor(options) {
     this.options = options || {};
+    this.container = null;
   }
 
   getFormFieldConfig(formFieldType) {
@@ -74,10 +78,16 @@ class CookieMatic {
    * @param {CookieConfig} param0
    */
   savePreferences({ analytics, marketing }) {
+    setCookie("consent_given", "1", 90);
     if (analytics) setCookie("consent_analytics", "1", 30);
     else setCookie("consent_analytics", "", 0);
     if (marketing) setCookie("consent_marketing", "1", 30);
     else setCookie("consent_marketing", "", 0);
+
+    this.hideSettings();
+    if (this.options.reload) {
+      window.location.reload();
+    }
   }
 
   /**
@@ -98,7 +108,7 @@ class CookieMatic {
    * @returns {String}
    */
   get html() {
-    return `<div class="cookiematic--drop">
+    return `<div class="cookiematic--drop cookiematic--drop-settings">
     <div class="cookiematic--banner">
       <h1>Nastavení cookies</h1>
       <div>
@@ -152,7 +162,9 @@ class CookieMatic {
         <div class="cookiematic--controls">
           <button
             class="cookiematic--button cookiematic--accept-all"
-            style="background-color: ${this.options.accentColor || "#18ad71d0"}"
+            style="background-color: ${
+              this.options.accentColor || "#18ad71d0"
+            }; color: ${this.options.accentText || "black"}"
             type="button"
           >
             Přijmout vše
@@ -178,11 +190,218 @@ class CookieMatic {
   }
 
   /**
+   * @returns {String}
+   */
+  get policyHtml() {
+    return `<div class="cookiematic--drop cookiematic--drop-policy">
+    <button class="cookiematic--close" type="button">&times;</button><div class="cookiematic--policy">
+    <div class="cookiematic--container">
+    <h1>Zásady zpracování osobních údajů ${
+      this.options.site || window.location.host
+    }</h1>
+    <p>
+    Všichni v naší firmě považujeme osobní údaje za velmi cenné
+    informace a podle toho s nimi také nakládáme. Prosíme přečtěte
+    si, jak k ochraně osobních údajů přistupujeme. Pokud byste s
+    jakýmkoliv bodem nesouhlasili nebo měli jakékoliv dotazy,
+    kontaktujte nás.
+  </p>
+
+  <h2>Správce osobních údajů</h2>
+
+  <p>Správcem osobních údajů je:</p>
+
+  <p style="text-align: center;">
+    Stock Plzeň - Božkov s.r.o.<br />Palírenská 641/2<br />326 00,
+    Plzeň - Božkov
+  </p>
+
+  <p style="text-align: center;">
+    Tel.: +420 378 081 111<br />Fax: +420 378 081 499<br />E-mail:
+    <a href="mailto:info@bozkov.cz">info@bozkov.cz</a>
+  </p>
+
+  <p style="text-align: center;">IČO: 27904636<br />DIČ: CZ27904636</p>
+
+  <p style="text-align: center;">
+    Zapsána v OR vedeném Kraj. Soudem v Plzni, oddíl C, vložka 21004
+  </p>
+
+  <h2>Účely zpracování</h2>
+
+  <p>Vaše osobní údaje zpracováváme pro následující účely:</p>
+
+  <ol>
+    <li>
+      Marketingové účely spočívající v&nbsp;individualizovaně cílené
+      reklamě (prostřednictvím cookies)
+    </li>
+    <li>
+      Datum narození poskytnuté při navštívení webové stránky je
+      vyžadováno z&nbsp;důvodu ochrany zdraví nezletilých před
+      vlivem návykových látek, přičemž nezletilým není dále vstup
+      povolen. Následně se do cookies spolu s&nbsp;IP adresou ukládá
+      pouze informace, zda jste starší 18 let.
+    </li>
+    <li>
+      V&nbsp;případě, že jste se na webové stránky ${
+        this.options.site || window.location.host
+      }
+      přihlásili prostřednictvím Facebook, pro účely přihlášení jsou
+      zpracovány Vaše osobní údaje (jméno, příjmení, e-mailová
+      adresa, věková hranice). Přihlašovací údaje jsou uloženy ve
+      formě autentikačního tokenu Facebook.
+    </li>
+    <li>
+      Bezchybné fungování našich webových stránek
+      <a href="https://www.${this.options.site || window.location.host}/">www.${
+      this.options.site || window.location.host
+    }</a> (tzv.
+      funkční cookies)
+    </li>
+  </ol>
+
+  <h2>Právní základ pro zpracování</h2>
+
+  <p>
+    Právním základem pro zpracování pro marketingové účely je
+    souhlas, který jste nám udělili při první návštěvě našich
+    webových stránek www.${
+      this.options.site || window.location.host
+    }. Poskytování osobních údajů je
+    smluvním požadavkem. Zpracování Vašich osobních údajů pro
+    marketingové účely prostřednictvím cookies není nezbytné pro
+    vstup a prohlížení našich webových stránek a souhlas
+    k&nbsp;tomuto účelu může být kdykoliv odvolán.
+  </p>
+
+  <p>
+    Datum narození a Vaše věková hranice jsou naopak zpracovávány na
+    základě zákonného požadavku (§ 15 zákona č. 65/2017 Sb., o
+    ochraně zdraví před škodlivými účinky návykových látek,
+    v&nbsp;platném znění), a proto je zpracování pro účely zjištění
+    vašeho věku nutností. Datum narození se nikde dále neuchovává a
+    slouží pouze k&nbsp;ověření věku pro vstup na webové
+    stránky&nbsp;<a href="http://www.${
+      this.options.site || window.location.host
+    }/">www.${this.options.site || window.location.host}</a>.
+  </p>
+
+  <p>
+    V&nbsp;případě, že zvolíte přihlášení přes Facebook, poskytujete
+    na základě smluvního požadavku též své jméno, příjmení a
+    e-mailovou adresu. Právním základem je Váš souhlas. Pokud nebude
+    Váš souhlas dán, nelze zvolit přihlášení přes Facebook.
+  </p>
+
+  <p>
+    Vaše osobní údaje v&nbsp;rozsahu IP adresa, věkové hranice jsou
+    zpracovány do podoby tzv. funkčních cookies, a to na základě
+    oprávněného zájmu správce, kterým je řádné fungování webových
+    stránek.
+  </p>
+
+  <h2>Rozsah zpracovávaných údajů</h2>
+
+  <p>
+    Zpracováváme jen nezbytně nutné údaje, abychom Vám mohli
+    nabídnout kvalitní obsah na míru.
+  </p>
+
+  <ul>
+    <li>Historie navštívených stránek (IP adresa, cookies)</li>
+    <li>
+      Přihlášení přes Facebook (jméno, příjmení, e-mailová adresa,
+      věková hranice) uložených správcem ve formě autentikačního
+      tokenu
+    </li>
+    <li>
+      Navštívení webu
+      <a href="https://www.${this.options.site || window.location.host}/">www.${
+      this.options.site || window.location.host
+    }</a> (datum
+      narození)
+    </li>
+  </ul>
+
+  <h2>Příjemci osobních údajů</h2>
+
+  <p>
+    Vaše osobní údaje chráníme jako oko v hlavě a&nbsp;<strong
+      >nikomu třetímu je neprodáme!</strong
+    >&nbsp;Abychom Vám mohli nabízet ten nejlepší obsah přesně na
+    míru, tak používáme kvalitní nástroje třetích stran. Tyto
+    nástroje některé Vaše osobní údaje zpracovávají - ale výhradně
+    za účelem cíleného oslovení nebo pro zlepšení kvality našich
+    služeb. Jejich výčet je v tabulce níže.
+  </p>
+
+  <figure class="wp-block-table">
+    <table class="">
+      <thead>
+        <tr>
+          <td>Název</td>
+          <td>Země</td>
+          <td>Účel zpracování</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>
+            <a
+              target="_blank"
+              href="https://www.google.com/analytics/"
+              rel="noreferrer noopener"
+              >Google Analytics</a
+            >
+          </th>
+          <td>US</td>
+          <td>Analýza chování na webu ${
+            this.options.site || window.location.host
+          }</td>
+        </tr>
+        <tr>
+          <th>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/"
+              rel="noreferrer noopener"
+              >Facebook</a
+            >
+          </th>
+          <td>US</td>
+          <td>Cílená reklama, analýza chování na webu</td>
+        </tr>
+        <!--<tr>
+          <th>
+            <a
+              target="_blank"
+              href="https://www.twitter.com/"
+              rel="noreferrer noopener"
+              >Twitter</a
+            >
+          </th>
+          <td>US</td>
+          <td>
+            Sdílení (pokud kliknete na webové stránce&nbsp;<a
+              href="http://www.${this.options.site || window.location.host}/"
+              >www.${this.options.site || window.location.host}</a
+            >&nbsp;na odkaz Twitter, budete přesměrováni na profil
+            společnosti Stock Plzeň - Božkov s.r.o. na Twitteru)
+          </td>
+        </tr>-->
+      </tbody>
+    </table>
+  </figure></div></div></div>`;
+  }
+
+  /**
    * create cookie banner elements in a container
    * @param {Element} container element to replace
    */
   create(container) {
-    container.innerHTML = this.html;
+    this.container = container;
+    this.container.innerHTML = `${this.html} ${this.policyHtml}`;
     document.querySelector(`.cookiematic--accept-all`).onclick = () => {
       this.savePreferences({ marketing: true, analytics: true });
     };
@@ -192,5 +411,42 @@ class CookieMatic {
     document.querySelector(`.cookiematic--accept-form`).onclick = () => {
       this.savePreferencesForm();
     };
+    document.querySelector(`.cookiematic--float`).onclick = () => {
+      this.showSettings();
+      return false;
+    };
+    document.querySelector(".cookiematic--drop-policy").onclick = () =>
+      this.hidePolicy();
+    document.querySelector(".cookiematic--close").onclick = () =>
+      this.hidePolicy();
+  }
+
+  #show(suffix) {
+    document
+      .querySelector(`.cookiematic--drop-${suffix}`)
+      .classList.add("show");
+  }
+
+  #hide(suffix) {
+    document
+      .querySelector(`.cookiematic--drop-${suffix}`)
+      .classList.remove("show");
+  }
+
+  showSettings() {
+    if (this.container) this.create(this.container);
+    this.#show("settings");
+  }
+
+  hideSettings() {
+    this.#hide("settings");
+  }
+
+  showPolicy() {
+    this.#show("policy");
+  }
+
+  hidePolicy() {
+    this.#hide("policy");
   }
 }
